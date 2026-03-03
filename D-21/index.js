1bfcf56cc6380cada46c0f29aba2970236c6bc02
ganeshshit <gsb021@nist.edu>
2026-03-03 12:35:51 +0530

    Day -21

---
+++ D-21/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app=express()
+const PORTlet orders = [
+    { id: 1, userId: 1, productId: 2, quantity: 1 },
+    { id: 2, userId: 1, productId: 3, quantity: 2 },
+    { id: 3, userId: 2, productId: 1, quantity: 1 }
+];
+app.listen(PORT, () => {
+    
+})
